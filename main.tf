c4e1f7ea4e19596d2f11056f42b14d295021d48c
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-25 17:36:29 +0200

    Add dynamic values

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 locals {
-  main_image = regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.main)
+  main_image      = regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.main)
+  main_pre_value  = "image"
+  main_set_values = local.main_image != {} ? [{ name = "${local.main_pre_value}.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "${local.main_pre_value}.tag", value = local.main_image.tag }] : []
+
+  set_values = concat(var.set_values, local.main_set_values)
 }
 
 resource "helm_release" "this" {
@@ -12,18 +16,25 @@ resource "helm_release" "this" {
 
   values = var.values
 
-  set {
-    name  = "image.repository"
-    value = "${local.main_image.url}/${local.main_image.image}"
+  dynamic "set" {
+    iterator = each_item
+    for_each = local.set_values
+
+    content {
+      name  = each_item.value.name
+      value = each_item.value.value
+      type  = try(each_item.value.type, null)
+    }
   }
 
-  set {
-    name  = "image.tag"
-    value = local.main_image.tag
-  }
+  dynamic "set_sensitive" {
+    iterator = each_item
+    for_each = var.set_sensitive_values
 
-  set {
-    name  = "serviceAccount.name"
-    value = "metrics-server"
+    content {
+      name  = each_item.value.name
+      value = each_item.value.value
+      type  = try(each_item.value.type, null)
+    }
   }
 }
