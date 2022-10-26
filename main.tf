9d25e5cf3e18b91089f3be55730009b31b43798e
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-26 10:29:20 +0200

    Tag module

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ locals {
 }
 
 module "helm" {
-  source               = "github.com/terraform-helm/terraform-helm"
+  source               = "github.com/terraform-helm/terraform-helm?ref=0.1"
   helm_config          = local.helm_config
   set_values           = local.set_values
   set_sensitive_values = var.set_sensitive_values
