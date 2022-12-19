ab16cdae3ebe50cd3eb1f964e4aba194740a5ed7
Joseph Ligier <joseph.ligier@accenture.com>
2022-12-19 10:02:05 +0100

    Improve code

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 locals {
-  main_image      = regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.main)
-  main_pre_value  = "image"
-  main_set_values = local.main_image != {} ? [{ name = "${local.main_pre_value}.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "${local.main_pre_value}.tag", value = local.main_image.tag }] : []
-
-  set_values = concat(var.set_values, local.main_set_values)
+  set_values = concat(var.set_values, module.main_image.set_values)
 
   default_helm_config = {
     name             = var.name
@@ -14,7 +10,15 @@ locals {
     version          = var.release_version
     values           = var.values
   }
+
   helm_config = merge(local.default_helm_config, var.helm_config)
+}
+
+module "main_image" {
+  source     = "github.com/littlejo/terraform-helm-images-set-values"
+  repo_regex = var.repo_regex
+  repo_url   = var.images.main
+  pre_value  = "image"
 }
 
 module "helm" {
